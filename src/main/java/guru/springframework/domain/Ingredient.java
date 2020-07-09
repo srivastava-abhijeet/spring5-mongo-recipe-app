ff049893acd4fe8b246f7d75c3939a23b473cc0f
Abhijeet <abhijeet.millenium@gmail.com>
2020-07-09 12:43:29 -0600

    fixed issues while updating recipe.

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by jt on 6/13/17.
@@ -14,16 +15,15 @@ import java.math.BigDecimal;
 @Setter
 public class Ingredient {
 
-    @Id
-    private String id;
+    private String id = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
 
     @DBRef
     private UnitOfMeasure uom;
-    //private Recipe recipe;
 
     public Ingredient() {
+
     }
 
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
@@ -36,7 +36,6 @@ public class Ingredient {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
-        //this.recipe = recipe;
     }
 
 }
